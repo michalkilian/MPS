2f55eb49098af122291e386729d12cf21ba1119e
michalkilian <mkilian@mielec.com.pl>
2021-05-15 00:25:31 +0200

    Change main class to extend the application class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import logic.Junction;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        primaryStage.setTitle("MPS Projekt");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
         Junction junction = new Junction(180, 210, 30);
         junction.grid.printGrid();
+
     }
 }
